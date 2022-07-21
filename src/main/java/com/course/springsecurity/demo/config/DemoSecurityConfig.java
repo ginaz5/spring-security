3fb0509fd820b53a3b890b4ea5220891f72e2f3c
Gina Lin <ginalin@alum.ccu.edu.tw>
2022-07-22 00:01:26 +0800

    create a custom login form

--- src/main/java/com/course/springsecurity/demo/config/DemoSecurityConfig.java
+++ src/main/java/com/course/springsecurity/demo/config/DemoSecurityConfig.java
@@ -2,6 +2,7 @@ package com.course.springsecurity.demo.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
@@ -18,5 +19,16 @@ public class DemoSecurityConfig extends WebSecurityConfigurerAdapter {
                 .withUser(users.username("john").password("test123").roles("EMPLOYEE"))
                 .withUser(users.username("mary").password("test123").roles("MANAGER"))
                 .withUser(users.username("susan").password("test123").roles("ADMIN"));
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+            http.authorizeRequests()
+               .anyRequest().authenticated()
+               .and()
+               .formLogin()
+               .loginPage("/showMyLoginPage")  // request mapping to our login page
+               .loginProcessingUrl("/authenticateTheUser") // login form should POST data to this URL for check user id and pwd
+               .permitAll(); // allow everyone to see login page w/o being logged in
     }
 }
