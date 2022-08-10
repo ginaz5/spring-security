2a7c49ca10871d16a92b4341c4b76438d9e26d0e
Gina Lin <ginalin@alum.ccu.edu.tw>
2022-08-10 09:49:22 +0800

    add JDBC authentication

--- src/main/java/com/course/springsecurity/demo/config/DemoSecurityConfig.java
+++ src/main/java/com/course/springsecurity/demo/config/DemoSecurityConfig.java
@@ -1,5 +1,6 @@
 package com.course.springsecurity.demo.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -7,18 +8,31 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
 
+import javax.sql.DataSource;
+
 @Configuration
 @EnableWebSecurity
 public class DemoSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    // add a reference to our security data source
+    @Autowired
+    private DataSource securityDataSource;
+
+
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
 
-        // add users for in memory authentication
+        // use jdbc authentication
+        auth.jdbcAuthentication().dataSource(securityDataSource);
+
+        /* users for in memory authentication
         User.UserBuilder users = User.withDefaultPasswordEncoder();
         auth.inMemoryAuthentication()
                 .withUser(users.username("john").password("test123").roles("EMPLOYEE"))
                 .withUser(users.username("mary").password("test123").roles("EMPLOYEE", "MANAGER"))
                 .withUser(users.username("susan").password("test123").roles("EMPLOYEE","ADMIN"));
+         */
+
     }
 
     @Override
